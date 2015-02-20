d3733db016846dc4eb801e54b0501bf289a3228a
Zachary Scott <e@zzak.io>
2015-02-20 14:43:32 -0800

    Include default gembox in travis build

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -2,6 +2,8 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   enable_debug
 
+  conf.gembox 'default'
+
   conf.gem :github => 'iij/mruby-io'
   #conf.gem :github => 'iij/mruby-dir'
   conf.gem :github => 'iij/mruby-pack'
