eda786b35f1fc8d4726214bcd4318fa5f3b6cd04
Zachary Scott <e@zzak.io>
2015-08-27 09:08:50 -0400

    Move files from development to example site for testing and docs

---
+++ example/site.rb
@@ -0,0 +1,7 @@
+Static.configure do |config|
+  config.site_name = "Static HTML Site"
+  config.root = "src"
+  config.asset_dir = "assets"
+  config.template_dir = "templates"
+  config.output = "output/"
+end
---
+++ example/src/hello.md
@@ -0,0 +1,18 @@
+## Hello, world!
+
+Here's a paragraph.
+
+* And
+* Here
+* Is
+* A List
+
+We can also show code:
+
+```
+def omg(hello="you")
+  puts "Hello, #{hello}!"
+end
+```
+
+Wasn't that amazing??
